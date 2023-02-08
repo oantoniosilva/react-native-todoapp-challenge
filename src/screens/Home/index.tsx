7ca9991b94109a031e6c89422bd339555353fb1c
Antonio <antoniopsilva083@gmail.com>
2023-02-07 23:51:44 -0300

    refact: update home and delete taskform

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,41 +1,105 @@
-import { View, Text, ScrollView } from "react-native";
+import { useState } from "react";
+import { Image, View, Alert, Text, TouchableOpacity, TextInput, FlatList } from "react-native";
+import { PlusCircle } from "phosphor-react-native";
 
 import { Header } from "../../components/Header";
-import { TaskEmpty } from "../../components/TaskEmpty";
-import { TaskForm } from "../../components/TaskForm";
 import { Tasks } from "../../components/Tasks";
+
 
 import { styles } from './styles';
 
-export function Home() {
-  const tasks = [
-    'Estudar Rust',
-    'Estudar Java',
-    'Estudar Javascript',
-    'Estudar Html',
-    'Estudar Css',
-    'Estudar Git',
-    'Estudar Springboot',
-  ]
+import clipboardImg from "../../assets/clipboard.png";
 
-  function handleTaskRemove() {
-    console.log('Você removeu uma task!')
+export function Home() {
+  const [tasks, setTasks] = useState<string[]>([])
+  const [taskName, setTaskName] = useState('')
+
+  function handleTaskAdd() {
+    if (tasks.includes(taskName)) {
+      return Alert.alert('Já existe uma task na lista com o mesmo nome!')
+    }
+
+    setTasks(prevState => [...prevState, taskName])
+    setTaskName('')
+
+  }
+
+  function handleTaskRemove(content: string) {
+
+    Alert.alert('Remover', `Você removeu a task ${content}?`, [
+      {
+        text: 'Sim',
+        onPress: () => setTasks(prevState => prevState.filter(task => task !== content))
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
   }
 
   return (
     <View style={styles.container}>
       <Header />
-      <TaskForm />
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {
-          tasks.map(task => (
-            <Tasks key={task} content={task} onRemove={handleTaskRemove} />
-          )) 
-        }
-      </ScrollView>
+      <View style={styles.form}>
+        <TextInput style={styles.input}
+          placeholder="Add a new task"
+          placeholderTextColor="#808080"
+          // onChangeText={event => setTaskName(event)}
+          onChangeText={setTaskName}
+          value={taskName}
+        />
 
-      {/* <TaskEmpty /> */}
+        <TouchableOpacity style={styles.button} onPress={handleTaskAdd}>
+          <PlusCircle size={16} color="#F2F2F2"/>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.counterContainer}>
+        <View style={styles.containerCounterCreate}>
+          <Text style={styles.textCreate}>Create</Text>
+
+          <View style={styles.counter}>
+            <Text style={styles.textCounter}>0</Text>
+          </View>
+        </View>
+
+        <View style={styles.containerCounterCompleted}>
+          <Text style={styles.textCompleted}>Completed</Text>
+
+          <View style={styles.counter}>
+            <Text style={styles.textCounter}>2</Text>
+          </View>
+        </View>
+      </View>
+
+      <FlatList
+        data={tasks}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Tasks
+            key={item}
+            content={item}
+            onRemove={() => handleTaskRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <View style={styles.container}>
+            <View style={styles.content}>
+              <Image 
+                source={clipboardImg}
+              />
+
+              <Text style={styles.text1}>
+                Você ainda não tem tarefas cadastradas
+              </Text>
+              <Text style={styles.text2}>Crie tarefas e organize seus itens a fazer</Text>
+            </View>
+          </View>
+        )}
+      />
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -7,4 +7,76 @@ export const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#1A1A1A',
   },
+  input: {
+    width: 271,
+    minHeight: 54,
+    marginRight: 4,
+    padding: 16,
+    backgroundColor: '#333333',
+    color: '#F2F2F2',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: '#0D0D0D',
+    borderRadius: 6,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 52,
+    height: 52,
+    padding: 18,
+    borderRadius: 6,
+    backgroundColor: '#1E6F9F',
+  },
+  form: {
+   flexDirection: 'row',
+   justifyContent: 'center',
+   alignItems: 'center',
+   marginTop: -30
+  },
+  counterContainer: {
+    width: '86%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 30
+  },
+  containerCounterCreate: {
+    flexDirection: 'row'
+  },
+  textCreate: {
+    color: '#4EA8DE'
+  },
+  counter: {
+    width: 25,
+    height: 19,
+    backgroundColor: '#333333',
+    borderRadius: 999,
+    alignItems: 'center',
+    marginLeft: 10
+  },
+  textCounter: {
+    color: '#F2F2F2'
+  },
+  containerCounterCompleted: {
+    flexDirection: 'row'
+  },
+  textCompleted: {
+    color: '#8284FA'
+  },
+  content: {
+    marginTop: 48,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text1: {
+    color: '#808080',
+    marginTop: 16,
+    fontWeight: 'bold'
+  },
+  text2: {
+    color: '#808080',
+    fontSize: 14,
+    marginTop: 1
+  }
 })
